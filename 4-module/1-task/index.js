0f43e674c9992ae9f5c51fe3d82eab73701994ae
Vlad <vladislav0000054@gmail.com>
2022-02-01 22:37:22 +0700

    add makeFriendsList function
    Функция `makeFriendsList`, которая преобразует переданный массив друзей в стандартный HTML список (`ul, li`).

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let html = document.createElement('ul');
+
+  for (const friend of friends) {
+    let child = document.createElement('li');
+    child.innerHTML = `${friend.firstName} ${friend.lastName}`;
+
+    html.appendChild(child);
+  }
+
+  return html;
 }
