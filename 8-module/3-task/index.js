36f256dc55a41192a729dbd4c4aadf1edea97fcc
Vlad <vladislav0000054@gmail.com>
2022-03-02 02:26:14 +0700

    add Cart class
    Компонент `Cart` ("Корзина") - отвечает за добавление и удаление товаров

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,33 +1,54 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+  cartItems = [];
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
   }
-
+  
   addProduct(product) {
-    // ваш код
+    if(product){
+      const cartItem = this.cartItems.find(x => x.product.id == product.id);
+      if(cartItem == null){
+        this.cartItems.push({product, count: 1});
+      }else{
+        cartItem.count++;
+      }
+  
+      this.onProductUpdate(cartItem);
+    }   
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const cartItem = this.cartItems.find(x => x.product.id == productId);
+    if(cartItem){
+      cartItem.count += amount;
+
+      if(cartItem.count === 0){
+        const cartItemItem = this.cartItems.indexOf(cartItem);
+        this.cartItems.splice(cartItemItem, 1);
+      }
+
+      this.onProductUpdate(cartItem);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return !this.cartItems.some(x => x.count > 0);
   }
 
   getTotalCount() {
-    // ваш код
+   return this.cartItems.reduce((sum, cartItem) => {
+      return sum + cartItem.count;
+    }, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((sum, cartItem) => {
+      return sum + cartItem.count * cartItem.product.price;
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
